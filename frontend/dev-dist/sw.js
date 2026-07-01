746b6a7971e09533c234834e0669c45b1a19197b
Danilo <danilohumberto12@hotmail.com>
2026-06-30 23:37:28 -0300

    fix: update revision number for precached index.html in service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.019gmcp1b"
+    "revision": "0.48d23lpskag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
